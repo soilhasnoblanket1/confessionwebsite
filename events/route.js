29816b1afe0a999b4187d05843cc739ff387a42f
soilhasnoblanket1 <166317388+soilhasnoblanket1@users.noreply.github.com>
2024-09-15 23:45:12 +0545

    Update route.js

--- events/route.js
+++ events/route.js
@@ -38,7 +38,7 @@ app.get("/", (req, res, next) => {
     res.render("index", {
       confessions: confessions,
       formatTimeDifference,
-      ip: ipAddress, 
+      ipAddress: ipAddress, 
     });
   });
 });
