2b9dcee8fb36f7aa0da7d20d8dc2a961eb042040
soilhasnoblanket1 <166317388+soilhasnoblanket1@users.noreply.github.com>
2024-09-29 14:55:15 +0545

    Update route.js

--- events/route.js
+++ events/route.js
@@ -8,7 +8,7 @@ const Comment = require("../models/comment.model");
 const Discord = require('discord.js');
 
 const discordConfig = {
-  token: process.env.TOKEN,
+  token: process.env.BOTTOKEN,
   clientId: '882161977009512448',
   guildId: '1141793551403978803',
   channelId: '1285234436560130151'
