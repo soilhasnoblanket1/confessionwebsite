2e4601adeedbf6f6dcace36487c66933f002134b
soilhasnoblanket1 <166317388+soilhasnoblanket1@users.noreply.github.com>
2024-10-01 18:08:10 +0545

    Update route.js

--- events/route.js
+++ events/route.js
@@ -270,14 +270,13 @@ app.post('/postimg/image', upload.single('image'), async (req, res) => {
     if (!file.path || !file.originalname) {
       throw new Error('Invalid file uploaded');
     }
-    const caption = req.body.caption; // Get the caption from the request body
     try {
       const buffer = await sharp(file.path).toBuffer();
       const messagePayload = {
         embeds: [
           {
             title: 'Image uploaded successfully!',
-            description: caption, // Use the caption here
+            description: 'fuck u saurav',
             image: {
               url: 'attachment://' + file.originalname
             }
